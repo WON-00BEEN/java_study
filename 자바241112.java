fbef0c47a88f975ac0974a977ec984960ade07fb
WON-00BEEN <iwonbin354@gmail.com>
2024-11-12 21:36:25 +0900

    Add files via upload

---
+++ 자바241112.java
@@ -0,0 +1,37 @@
+package java_study;
+
+public class 자바241112 {  //자바는 한글지원이 가능합니다.
+
+	public static void main(String[] args) {
+	         
+		
+		System.out.println("안녕하세요 오늘부터 자바공부를 시작.");
+	
+	    System.out.println("Hello world");
+	    
+	    System.out.println("아무말 대잔치");
+	    
+	    System.out.println("현재년도는    :  " +2024+ "입니다");
+	    
+	    System.out.println("  키   :  " +00+ "입니다");
+	    System.out.println("  나이  :  " +00+ "입니다");
+	   
+	    System.out.print("안녕하세요");
+	    System.out.print("안녕하세요");
+	    
+	    
+	    //변수
+	    int age = 20;
+	    String address = "대전 서구 둔산동";
+	    String mbti = "INTP";	
+	    
+	    System.out.println("나이는  :  "  +age);
+	    System.out.println("주소는  :  " +address);
+	    System.out.println("MBTI :  " + mbti);
+	// syso 스페이스바 ctrl+enter
+	}
+	
+	
+
+	
+}
---
+++ 자바복습.java
@@ -0,0 +1,27 @@
+package java_study;
+
+public class 자바복습 {
+
+	public static void main(String[] args) {
+	
+		String name = "이원빈";
+		int age = 25;
+	    String address = "경기도 용인시 기흥구 보라동";
+	    String hobby = "헬스";
+	    String mbti = "INTP";	
+	    
+	    System.out.println("이름  : " + name);
+	    System.out.println("나이  : " + age);
+	    System.out.println("주소  : " + address);
+	    System.out.println("취미  : " + hobby);
+	    System.out.println("Mbti  : " + mbti);
+	    
+	    
+	    Scanner scan = new scanner (system.in);
+	    System.out.print("이름을 입력해주세요  => ");
+	    String n = scan next();
+	    System.out.print("입력하신 이름은 +n+ 입니다. ");
+	    
+	}
+
+}
